0ac4e64f0b6e89388107c7df88988ae38d99b07c
rodneywardle <rodwardle@hotmail.com>
2015-04-29 00:43:53 +0100

    deleted

--- json.js
+++
@@ -1,8 +0,0 @@
-var text = '{"Title":"Shark Stories","Date":"15 December 2015- 18th December 2015","phone":"555 1234567"}'
-
-var obj = JSON.parse(text);
-
-document.getElementById("demo").innerHTML =
-    obj.Title + "<br>" +
-    obj.Date + "<br>" +
-    obj.phone;

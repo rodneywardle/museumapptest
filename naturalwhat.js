8513ffee47de439266c2b4e8c3d0c9a7d0869ac7
rodneywardle <rodwardle@hotmail.com>
2015-04-29 01:56:29 +0100

    NATURAL

--- naturalwhat.js
+++ naturalwhat.js
@@ -33,7 +33,6 @@ var info = [{
 
 }];
 
-var json = '{"glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","ID":"44","str":"SGML","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}';
 
 
 function getValues(obj, key) {
